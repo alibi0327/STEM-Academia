f1284e3f1395b787e69cec61c183c8605e23cfac
Алиби <alibiit03@gmail.com>
2026-08-26 19:36:48 +0500

    Create users.js

---
+++ data/users.js
@@ -0,0 +1,30 @@
+const STEM_USERS = [
+
+    {
+        id: 1,
+        fullName: "Тестовый Учитель",
+        login: "teacher1",
+        password: "12345",
+
+        role: "teacher",
+
+        courses: [
+            "Labdisc"
+        ]
+    },
+
+    {
+        id: 2,
+        fullName: "Учитель №2",
+        login: "teacher2",
+        password: "54321",
+
+        role: "teacher",
+
+        courses: [
+            "Labdisc",
+            "Arduino"
+        ]
+    }
+
+];
